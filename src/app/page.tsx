2180d243e04f5917fc4ab3d3f597d7d0b83c7553
lucidmach <lucidmach@gmail.com>
2025-05-06 22:05:21 +1000

    responsive but not as per design [designs not finished]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="bg-white h-full p-4">
+    <div className="bg-black h-full p-4">
       <NavBar />
     </div>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,28 +2,30 @@ import Image from "next/image";
 
 const NavBar: React.FC = () => {
   return (
-    <div className="p-9 bg-black rounded-3xl flex flex-row justify-between items-center w-full  gap-5 border-2 border-[#2C2C2D]">
+    <div className="md:p-9 p-3 bg-[#0000004B] rounded-3xl flex flex-row justify-between items-center w-full  gap-5 border-2 border-[#2C2C2D]">
       <div className="flex flex-row gap-6 items-center">
         <Image
           width={128}
           height={128}
-          className="w-14 h-14"
+          className="md:w-16 md:h-16 w-12 h-12"
           src="/assets/logo.png"
           alt="3d cube that has an illusion of having overlapping sides"
         />
         <div className="flex flex-col">
           <h1 className="text-3xl">MNET</h1>
-          <h2 className="">MONASH NEXUS FOR EMERGING TECHNOLOGIES</h2>
+          <h2 className="md:flex hidden">
+            MONASH NEXUS FOR EMERGING TECHNOLOGIES
+          </h2>
         </div>
       </div>
       <div className="flex flex-row gap-6 items-center">
-        <button className="hover:cursor-pointer h-fit px-8 py-2 bg-[#DC003B] rounded-md">
+        <button className="hover:cursor-pointer md:flex hidden h-fit px-8 py-3 bg-[#DC003B] rounded-md">
           CONTACT
         </button>
         <Image
           src="/assets/menu.svg"
-          width={64}
-          height={64}
+          width={40}
+          height={40}
           alt="hamberguer icon"
         ></Image>
       </div>
