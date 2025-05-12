144c7f845d30b02fb7c6166160670bf45d59acbb
lucidmach <lucidmach@gmail.com>
2025-05-13 00:13:08 +1000

    basic menu design - todo colourful border

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 const NavBar: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="m-4 md:p-7 p-3 z-10 bg-[#00050] rounded-3xl flex flex-row justify-between items-center gap-5 border-2 border-[#2C2C2D] backdrop-blur-md sticky top-0">
       <div className="flex flex-row gap-6 items-center">
@@ -25,6 +31,9 @@ const NavBar: React.FC = () => {
           CONTACT
         </button>
         <Image
+          onClick={() => {
+            setOpen((prev) => !prev);
+          }}
           className="hover:cursor-pointer"
           src="/img/menu.svg"
           width={40}
@@ -32,6 +41,22 @@ const NavBar: React.FC = () => {
           alt="hamburger icon"
         ></Image>
       </div>
+      {open ? (
+        <div className="flex flex-col absolute right-4 top-36 bg-black rounded-3xl py-4 px-8 gap-4 font-offbit font-bold text-2xl items-center border-2 border-[#2C2C2D]">
+          <Link className="hover:opacity-100 opacity-75" href={"/"}>
+            Home
+          </Link>
+          <Link className="hover:opacity-100 opacity-75" href={"/projects"}>
+            Projects
+          </Link>
+          <Link className="hover:opacity-100 opacity-75" href={"/events"}>
+            Events
+          </Link>
+          <Link className="hover:opacity-100 opacity-75" href={"/about-us"}>
+            About Us
+          </Link>
+        </div>
+      ) : null}
     </div>
   );
 };
