c7626dce403d5ab2cc9e7ff386a45cacb70a8711
lucidmach <lucidmach@gmail.com>
2025-05-13 00:47:31 +1000

    menu mirco-sync up with figma

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,8 +10,8 @@ const NavBar: React.FC = () => {
   return (
     <div className="w-full h-full">
       {open ? (
-        <div className="absolute z-10 top-0 left-0 w-full h-full bg-black opacity-90">
-          <div className="absolute right-4 top-36 bg-gradient-to-b from-[#030CAB] to-[#DC003B] p-0.5 rounded-3xl">
+        <div className="absolute z-10 top-0 left-0 w-full h-full bg-black opacity-80">
+          <div className="absolute right-4 top-40 bg-gradient-to-b from-[#030CAB] to-[#DC003B] p-0.5 rounded-3xl">
             <div className="flex flex-col bg-black rounded-3xl py-4 px-8 gap-4 font-offbit font-bold text-2xl items-center border-2 border-[#2C2C2D]">
               <Link className="hover:opacity-100 opacity-75" href={"/"}>
                 Home
