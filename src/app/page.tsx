a0c4e1314729abc7cf1f0e08689ba47687cd009a
Guitarband <preetishchoudhary@gmail.com>
2025-05-16 23:42:26 +1000

    Split events part into card design and seperate holder for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import EventsHolder from "@/components/EventsHolder";
+import EventsHolder from "@/components/events/PastEvents_Home";
 
 export default function Home() {
   return (
     <>
       <div className="bg-black h-full p-4">
         <NavBar />
-        <EventsHolder variant={"gradient"} timing="past" page="home" />
+        <EventsHolder />
       </div>
       <Footer />
     </>
