44058cc11597c625eb05d6c9ac9169d51808bcbb
lucidmach <lucidmach@gmail.com>
2025-05-18 23:56:07 +1000

    dynamic countdown

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Hero: React.FC = () => {
   const latestEvent = {
     title: "MNET x MDN Tech Futures Industries",
-    timestamp: new Date(),
+    timestamp: new Date("2025-05-24T03:24:00").getTime(),
   };
+  // {
+  //   days: Math.floor(latestEvent.timestamp / (1000 * 60 * 60 * 24)),
+  //   hours: Math.floor(
+  //     (latestEvent.timestamp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //   ),
+  //   minutes: Math.floor(
+  //     (latestEvent.timestamp % (1000 * 60 * 60)) / (1000 * 60)
+  //   ),
+  //   seconds: Math.floor((latestEvent.timestamp % (1000 * 60)) / 1000),
+  // }
+
+  const [date, setDate] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timestamp = latestEvent.timestamp;
+    const now = new Date().getTime();
+    const countdown = timestamp - now;
+
+    setTimeout(() => {
+      setDate({
+        days: Math.floor(countdown / (1000 * 60 * 60 * 24)),
+        hours: Math.floor(
+          (countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ),
+        minutes: Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((countdown % (1000 * 60)) / 1000),
+      });
+    }, 1000);
+  });
+
   return (
     <div className="absolute top-0 bg-[url(/img/spacefabric.png)] bg-center bg-cover w-full h-full flex justify-center items-center">
       <div className="md:w-3/5 p-4 flex flex-col md:gap-0 gap-4">
         <p className="font-offbit font-bold md:text-2xl text-sm">
-          {latestEvent.title}: {latestEvent.timestamp.getDay()}d{" "}
-          {latestEvent.timestamp.getHours()}h{" "}
-          {latestEvent.timestamp.getMinutes()}m{" "}
-          {latestEvent.timestamp.getSeconds()}s
+          {latestEvent.title}: {date.days}d {date.hours}h {date.minutes}m{" "}
+          {date.seconds}s
         </p>
         <h1 className="font-offbit-dot font-bold md:text-7xl text-5xl">
           MONASH NEXUS FOR EMERGING TECHNOLOGIES
