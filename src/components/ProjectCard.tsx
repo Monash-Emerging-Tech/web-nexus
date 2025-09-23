4cffb90aa882ca1ef03dfb82733e0c538c082e98
miasud <msud0005@student.monash.edu>
2025-09-23 17:46:09 +1000

    Made description text bigger

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,12 +11,12 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageUrl, tags, githubUrl }) => {
   return (
     
-    <div   className=" rounded-[10px] bg-black shadow-lg overflow-hidden border border-[#DB003B] py-3.75 px-3 flex flex-col h-full">
+    <div   className=" rounded-[15px] bg-black shadow-lg overflow-hidden border border-[#DB003B] py-3.75 px-3 flex flex-col h-full">
       {/* Title */}
       <h3 className="font-offbit-dot font-bold text-xl text-white ">{title}</h3>
 
       {/* Description */}
-      <p className="font-offbit text-white text-sm  flex-grow">{description}</p>
+      <p className="font-offbit text-white text-lg  flex-grow">{description}</p>
       {/* Image Container */}
       <div style={{ aspectRatio: '384.28 / 213.27', }} className="w-auto  mb-3 overflow-hidden ">
         <img src={imageUrl} alt={title} className="w-full h-full object-cover " />
