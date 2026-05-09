dc1773917548d688fb22518bdf0b20c37fce2a72
lucidmach <lucidmach@gmail.com>
2026-05-09 22:08:19 +1000

    navbar in all places

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { offbit, offbit101, offbitDot } from "@/lib/fonts";
 import "./globals.css";
 import { NavBarProvider } from "@/components/navbar_test/NavProvider";
+import Footer from "@/components/Footer";
+import Nav from "@/components/navbar_test/Nav";
 
 export const metadata: Metadata = {
   title: "Monash Nexus for Emerging Technologies",
@@ -16,14 +18,21 @@ export default function RootLayout({
   return (
     <html lang="en" className="w-full h-full">
       <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        />
         <link rel="icon" href="/img/favicon.ico" sizes="any" />
       </head>
       <body
         className={`${offbit.variable} ${offbit101.variable} ${offbitDot.variable} antialiased w-full h-auto`}
       >
         <NavBarProvider>
-          {children}
+          <div className="bg-black min-h-screen w-full">
+            <Nav />
+            {children}
+            <Footer />
+          </div>
         </NavBarProvider>
       </body>
     </html>
