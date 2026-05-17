954284995aceb76a5fbf4926417816848787ad37
Christina Wang <christinawang@Mac.lan>
2026-05-17 20:24:47 +1000

    Setup projects card template and draft projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,39 @@
 // import { getActivePortfolios } from "@/lib/notion/portfolios";
+import Footer from "@/components/Footer";
+import Nav from "@/components/navbar_test/Nav";
+import ProjectsCard from "./ProjectsCard";
 
-// export const revalidate = 86400;
 
 export default async function Projects() {
   // const data = await getActivePortfolios();
-  return <div>This is the projects page</div>;
+  return (
+    <>
+    <div className="Nav">
+      {/* <Nav /> */}
+      <div>This is the projects page</div>
+      <div className="project-grid">
+    </div>
+    </div>
+    <>
+    </>
+    <div className="grid grid-cols-1 md:grid-cols-2 p-6 bg-black">
+
+      {/* Project Card Heading */}
+      <ProjectsCard />
+      <ProjectsCard />
+      <ProjectsCard />
+      <ProjectsCard />
+      
+
+      {/* Repeat for other cards... */}
+    </div>
+
+
+    <div className="Footer">
+      <Footer />
+    </div>
+    </>
+  );
 }
+
+
